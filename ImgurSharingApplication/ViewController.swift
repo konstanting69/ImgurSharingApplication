d7a5f703648f5a9b88a8669c43cfcd4116a4c098
Konstantin <konstanting69@gmail.com>
2020-11-07 13:26:37 +0000

    Initial Commit

---
+++ ImgurSharingApplication/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImgurSharingApplication
+//
+//  Created by Konstantin Georgiev on 07/11/2020.
+//  Copyright © 2020 Konstantin Georgiev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
